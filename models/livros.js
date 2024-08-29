e61b942ed1c9b459e929343c805300e780d8888e
kauanysc16 <kauanycamilo91@gmail.com>
2024-08-29 16:57:47 -0300

    first commit

---
+++ models/livros.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const livroSchema = new Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    autor: {
+        type: String,
+        required: true
+    },
+    ano: {
+        type: Number,
+        required: true
+    },
+    genero: {
+        type: String,
+        required: false
+    }
+});
